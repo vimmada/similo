140cd178f1dffdaa0e66bab4943036c860a643f4
Vamsi <vimmada@DESKTOP-QEHFJMJ.localdomain>
2018-02-20 21:54:03 -0500

    Finished sql schema and database script

--- server/sql/data.sql
+++ server/sql/data.sql
@@ -1,2 +1,7 @@
-INSERT INTO users(username, fullname, email, password) VALUES ('Vamsi', 'Vamsi Nimmagadda', 'vimmada@gmail.com', "vamsi100");
-INSERT INTO items(itemid, description, link) VALUES (3, 'State of the art xbox', "www.xbox.com");
+INSERT INTO users(fullname, email, password) VALUES ('Vamsi Nimmagadda', 'vimmada@gmail.com', "vamsi100");
+INSERT INTO users(fullname, email, password) VALUES ('Perry Jiang', 'Perry@gmail.com', "perry100");
+INSERT INTO users(fullname, email, password) VALUES ('Kevin Yang', 'Kevin@gmail.com', "kevin100");
+INSERT INTO saved_items(description, item_title, price, link, userid) VALUES 
+                       ('latest xbox','xbox', 299.99,'www.xbox.com', 3);
+INSERT INTO saved_items(description, item_title, price, link, userid) VALUES 
+                       ('the newest iPhone','iPhone11', 750.50,'www.apple.com', 1);
--- server/sql/schema.sql
+++ server/sql/schema.sql
@@ -1,16 +1,23 @@
 CREATE TABLE users (
-  username VARCHAR(20) NOT NULL,
+  userid INTEGER PRIMARY KEY AUTOINCREMENT,
   fullname VARCHAR(40) NOT NULL,
   email VARCHAR(40) NOT NULL,
-  password VARCHAR(256) NOT NULL,
-  PRIMARY KEY(username)
+  password VARCHAR(256) NOT NULL
 );
 
-CREATE TABLE items (
-  itemid INTEGER PRIMARY KEY NOT NULL,
+CREATE TABLE saved_items (
+  itemid INTEGER PRIMARY KEY AUTOINCREMENT,
+  item_title TEXT NOT NULL,
   description VARCHAR(1000) NOT NULL,
-  owner VARCHAR(50),
   price REAL,
   link TEXT NOT NULL,
+  userid INTEGER NOT NULL,
+  FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE
 );
 
+CREATE TABLE history_items (
+  image BLOB NOT NULL,
+  userid INTEGER NOT NULL,
+  searched TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  FOREIGN KEY(userid) REFERENCES users(userid) ON DELETE CASCADE
+);

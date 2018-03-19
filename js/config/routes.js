3657ebb65aa76e1141d5f58d63284eb379255bd9
Perry Jiang <jiangperry@gmail.com>
2018-03-18 23:26:24 -0400

    Frontend dummy authentication

--- js/config/routes.js
+++ js/config/routes.js
@@ -1,8 +1,8 @@
-'use strict';
-
-import { StackNavigator } from 'react-navigation';
+import { StackNavigator, SwitchNavigator } from 'react-navigation';
 
 import AdvancedSearchScreen from '../containers/AdvancedSearchScreen';
+import AuthLoadingScreen from '../containers/AuthLoadingScreen';
+import AuthScreen from '../containers/AuthScreen';
 import BufferScreen from '../containers/BufferScreen';
 import FilterScreen from '../containers/FilterScreen';
 import HomeScreen from '../containers/HomeScreen';
@@ -12,7 +12,7 @@ import RecommendationsScreen from '../containers/RecommendationsScreen';
 import SavedItemsScreen from '../containers/SavedItemsScreen';
 import SettingsScreen from '../containers/SettingsScreen';
 
-const RootNavigator = StackNavigator(
+const AppNavigator = StackNavigator(
   {
     AdvancedSearch: {
       screen: AdvancedSearchScreen,
@@ -44,7 +44,20 @@ const RootNavigator = StackNavigator(
   },
   {
     initialRouteName: 'Home',
-  }
+  },
+);
+
+const AuthNavigator = StackNavigator({ Login: { screen: AuthScreen } });
+
+const RootNavigator = SwitchNavigator(
+  {
+    App: AppNavigator,
+    Auth: AuthNavigator,
+    AuthLoading: AuthLoadingScreen,
+  },
+  {
+    initialRouteName: 'AuthLoading',
+  },
 );
 
 export default RootNavigator;
